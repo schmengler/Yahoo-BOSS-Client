9dd9939ad74cac0e84573b281867a07baafdcea5
Fabian Schmengler <fschmengler@sgh-it.eu>
2010-10-14 18:46:18 +0200

    check allow_url_fopen requirement

--- YahooBossClient/YBC.lib.php
+++ YahooBossClient/YBC.lib.php
@@ -4,6 +4,9 @@ namespace YBC;
 if (version_compare(PHP_VERSION, '5.3', '<')) {
 	throw new Exception('The YBC package needs PHP 5.3.0 or higher. If you are using PHP 5.2 please use the YBC_Legacy package.');
 }
+if (!ini_get('allow_url_fopen')) {
+	throw new Exception('The YBC package relies on the allow_url_fopen setting. Please turn it on in php.ini or with ini_set(\'allow_url_fopen\', \'1\');');
+}
 
 /**
  * Autoload function
